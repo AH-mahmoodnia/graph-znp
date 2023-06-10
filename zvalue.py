ce82efc4fbdfbe4cb67c64e613dc4c9d9b3da98e
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-06-10 10:58:16 +0330

    add  some copy/pasting.

---
+++ zvalue.py
@@ -0,0 +1,23 @@
+def generate_lookup_table(dimensions):
+    if dimensions < 32:
+        limit = dimensions
+    else:
+        limit = 32
+    lookup_table = [[0]*256]*dimensions
+    for i in range(dimensions):
+        for j in range(256):
+            answer = 0
+            for k in range(limit):
+                answer |= ((j & (1 << k)) << (k*(dimensions-1) + i))
+            lookup_table[i][j] = answer
+    return lookup_table
+
+
+def z_value(lookup_table, point, dimensions):
+    answer = 0
+    rightshift = 32
+    for i1 in range(4):
+        answer = answer  << dimensions*8
+        for i2 in range(dimensions):
+            answer |= lookup_table[i2][(point[i2] >> (rightshift -8*i1)) & 0xFF ]
+    return answer
