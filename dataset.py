cc6ebe6da4e0f560f03d4c64aaeb78b8c4fe371a
amir mahmoodnia <mahmoodnia.am@gmail.com>
2023-06-10 16:43:06 +0330

    alireza sahaf

--- dataset.py
+++ dataset.py
@@ -3,16 +3,45 @@ class Dataset:
     def __init__(self, size, dimensionality):
         self.size = size
         self.dimensionality = dimensionality
-        self.vector_size = self.dimensionality * sizeof(float)
-        self.data = [] * self.size
+        self.data = [[0]*self.dimensionality] * self.size
+        self.strings
         self.string = None
 
+    def get_vector(self, idx):
+        return self.data[idx]
 
+    def find_min_max(self):
+        elem_min = float("inf")
+        elem_max = float("-inf")
+        print("Start find the min max")
+        for i in range(self.size):
+            v = get_vector(i)
+            for j in range(self.dimensionality):
+                if elem_min > v[j]:
+                    elem_min = v[j]
+                if elem_max < v[j]:
+                    elem_max = v[j]
+        print("end of find min max")
+        self.elem_min = elem_min
+        self.elem_max = elem_max
+
+    def set_val(self, data_i, elem_i, val):
+        self.data[data_i].insert(elem_i, val)
+
+    def get_val(self, data_i, elem_i):
+        return self.data[data_i][elem_i]
+
+    def dump_vector(self, idx):
+        v = get_vector(idx)
+        print(v)
+
+# p1_idx, p2_idx: points that we measure the distance between them
+# D: number of features, p: minkowski param 
 def minkowskiDist(p1_idx, p2_idx, D , p):
     tmp, dist = 0, 0
     for i in range(D):
-        tmp = p1_idx - p2_idx
+        tmp = p1_idx[i] - p2_idx[i]
         dist += abs(tmp) ** p
-        p1_idx += 1
-        p2_idx += 1
-    return math.sqrt(dist)
+    return dist ** (1/p)
+
+
